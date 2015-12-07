c2a835cf93db240724366413eddc73691318d746
Allen <Allen@zhangchuningdemacbook-pro.local>
2015-12-07 10:59:09 +0800

    Initial Commit

---
+++ MyWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeibo
+//
+//  Created by Allen on 15/12/7.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
